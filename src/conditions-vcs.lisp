5c70b076d2f9c186b18efc80d042561136bcc362
takeokunn <bararararatty@gmail.com>
2026-09-05 15:51:21 +0900

    docs: remove stale source comment navigation

--- src/conditions-vcs.lisp
+++ src/conditions-vcs.lisp
@@ -1,7 +1,7 @@
 (in-package #:vcs-kit)
 
 ;; Backend-neutral command failures. Backend-specific conditions remain
-;; available as the Git and GHQ operation contracts above.
+;; available as the Git and GHQ operation contracts.
 (define-condition vcs-command-error (vcs-error)
   ((repository
     :initarg :repository
